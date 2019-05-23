de6d9b43f87b7f189db421b4e8a214eb1db51951
Eduardo Alcantara de Oliveira <eduardo_alcantara@hotmail.com>
2019-05-23 17:56:56 -0300

    T4

--- Core/Domain/Entities/Usuario.cs
+++ Core/Domain/Entities/Usuario.cs
@@ -4,5 +4,8 @@ namespace Domain.Entities
     public class Usuario : Base
     {
         public string Login { get; set; }
+        public string Name { get; set; }
+        public int Password { get; set; }
+        public string Email { get; set; }
     }
 }
